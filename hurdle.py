d6c9ad7ea6666ffe7801286e1c8e5bee3daad792
Ardli Kafi Murobby <145179814+ardlikafi@users.noreply.github.com>
2023-11-16 12:01:29 +0700

    kode hurdle

---
+++ hurdle.py
@@ -0,0 +1,24 @@
+def menghindar():
+    while not wall_in_front():
+        move()
+        if at_goal():
+            done()
+    turn_left() 
+    while wall_on_right():
+        move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    while not wall_in_front():
+        move()
+    turn_left()
+    
+while not at_goal():
+    menghindar()
+        
+
+
